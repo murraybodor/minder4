d89b52bcd184635dc84aba0b7b308857b5281008
murraybodor <murray.bodor@gmail.com>
2016-09-19 10:25:27 -0600

    'First'

---
+++ src/main/java/hello/Controller.java
@@ -0,0 +1,44 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+
+	private static final Logger log = LoggerFactory.getLogger(Controller.class);
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+	private RestTemplate restTemplate;
+
+    @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    	restTemplate = builder.build(); 
+		return restTemplate;
+	}
+
+    @RequestMapping("/greeting")
+    public Greeting greetingRestService(@RequestParam(value="name", defaultValue="World") String name) {
+		log.info(String.format(template, name));
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+    @RequestMapping("/quote")
+    public Greeting quoteRestClient(@RequestParam(value="name", defaultValue="World") String name) {
+    	Quote quote = restTemplate.getForObject(
+				"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+		log.info(quote.toString());
+        return new Greeting(counter.incrementAndGet(),
+        		quote.toString());
+    }
+    
+}
